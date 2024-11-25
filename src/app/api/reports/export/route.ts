79aedba34fe568126328776e683cdb290ba1a87d
Harsh Nasit <84892689+hknasit@users.noreply.github.com>
2024-11-25 18:58:11 -0500

    Report Page Update

--- src/app/api/reports/export/route.ts
+++ src/app/api/reports/export/route.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 // app/api/reports/export/route.ts
 import { NextRequest, NextResponse } from "next/server";
 import mongoose from "mongoose";
